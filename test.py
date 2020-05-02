5357570b7ebca00af356c39e45f673884897e20e
renxian <xian1995106@gmail.com>
2020-05-03 01:25:11 +0200

    Primzahl

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("test")
+import math
+
+def func(n):
+    isFlag = True  # True = Primzahlen
+
+    for i in range(2, n):
+        for j in range(2, int(math.sqrt(i))):
+            if i % j == 0:
+                isFlag = False
+                break
+
+        if isFlag == True:
+            print(i)
+        isFlag = True
+
+num = 10000000
+print(func(num))
