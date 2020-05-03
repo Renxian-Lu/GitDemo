d91a2449960bb777c5ae2299359d1e07b4785cbd
renxian <xian1995106@gmail.com>
2020-05-03 11:27:48 +0200

    TestCode

--- test.py
+++ test.py
@@ -4,10 +4,12 @@ def func(n):
     isFlag = True  # True = Primzahlen
 
     for i in range(2, n):
-        for j in range(2, i//2):
+        j=2
+        while j <= math.sqrt(i):
             if i % j == 0:
                 isFlag = False
                 break
+            j += 1
 
         if isFlag == True:
             print(i)
